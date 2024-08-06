14843dfd84c94d664d882416d7d28f8e16cdb675
Insignificant74 <86426272+Insignificant74@users.noreply.github.com>
2024-08-06 10:04:00 +0100

    Correct Image resizing and more code cleanup

--- Assets/Wik_JS.js
+++ Assets/Wik_JS.js
@@ -1,8 +1,33 @@
 window.onload = function () {
-	const Index = document.querySelector('#Index');
-	var w = window.innerWidth;
+	const ImageLefts = document.querySelectorAll('.ImageLeft');
+	const ImageRights = document.querySelectorAll('.ImageRight');
+	
+	setToggleIndex;
 
-	if (w > 900){
+	ImageLefts.forEach(resizeBackground);
+	ImageRights.forEach(resizeBackground);
+
+	window.addEventListener('resize', function(event) {
+		ImageLefts.forEach(resizeBackground);
+		ImageRights.forEach(resizeBackground);
+	}, true);
+}
+
+function resizeBackground(element){
+	const elementHeight = element.offsetHeight;
+	var parentElement = element.parentElement;
+	
+	if (parentElement.nodeName == 'A'){
+		parentElement = parentElement.parentElement;
+	};
+
+	if (parentElement.offsetHeight < elementHeight) {
+		parentElement.style.height = `${elementHeight}px`;
+	}
+}
+function setToggleIndex(){
+	const Index = document.querySelector('#Index');
+	if (window.innerWidth > 900){
 		Index.addEventListener('mouseover', function () {
 			Index.classList.add('is-active');
 		});
@@ -16,9 +41,4 @@ window.onload = function () {
 			Index.classList.toggle('is-active');
 		});
 	}
-
-
-	window.addEventListener('resize', function(event) {
-
-	}, true);
 }
