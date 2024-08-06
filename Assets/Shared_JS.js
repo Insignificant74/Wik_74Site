c05337d87cf2368f761e2939b446b937ad443fa3
Insignificant74 <86426272+Insignificant74@users.noreply.github.com>
2024-08-06 10:34:08 +0100

    Seperated more files, added isaac site

---
+++ Assets/Shared_JS.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+	const ImageLefts = document.querySelectorAll('.ImageLeft');
+	const ImageRights = document.querySelectorAll('.ImageRight');
+	
+	if (window.innerWidth > 900){
+		ImageLefts.forEach(resizeBackground);
+		ImageRights.forEach(resizeBackground);
+		
+		window.addEventListener('resize', function(event) {
+			ImageLefts.forEach(resizeBackground);
+			ImageRights.forEach(resizeBackground);
+		}, true);
+	}
+	else{
+	}
+}
+
+function resizeBackground(element){
+	const elementHeight = element.offsetHeight;
+	var parentElement = element.parentElement;
+	
+	if (parentElement.nodeName == 'A'){
+		parentElement = parentElement.parentElement;
+	};
+
+	if (parentElement.offsetHeight < elementHeight) {
+		parentElement.style.height = `${elementHeight}px`;
+	}
+}
--- Assets/Wik_JS.js
+++ Assets/Wik_JS.js
@@ -1,7 +1,4 @@
 window.onload = function () {
-	const ImageLefts = document.querySelectorAll('.ImageLeft');
-	const ImageRights = document.querySelectorAll('.ImageRight');
-	
 	const Index = document.querySelector('#Index');
 	if (window.innerWidth > 900){
 		Index.addEventListener('mouseover', function () {
@@ -11,30 +8,10 @@ window.onload = function () {
 		Index.addEventListener('mouseout', function () {
 			Index.classList.remove('is-active');
 		});
-		ImageLefts.forEach(resizeBackground);
-		ImageRights.forEach(resizeBackground);
-		
-		window.addEventListener('resize', function(event) {
-			ImageLefts.forEach(resizeBackground);
-			ImageRights.forEach(resizeBackground);
-		}, true);
 	}
 	else{
 		Index.addEventListener('click', function () {
 			Index.classList.toggle('is-active');
 		});
-	}
-}
-
-function resizeBackground(element){
-	const elementHeight = element.offsetHeight;
-	var parentElement = element.parentElement;
-	
-	if (parentElement.nodeName == 'A'){
-		parentElement = parentElement.parentElement;
-	};
-
-	if (parentElement.offsetHeight < elementHeight) {
-		parentElement.style.height = `${elementHeight}px`;
 	}
 }
