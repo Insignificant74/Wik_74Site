96778724972b80cefa9fe4656867bd86227e8125
Insignificant74 <86426272+Insignificant74@users.noreply.github.com>
2024-08-07 18:49:01 +0100

    PHP should work but honestly who knows

--- update/index.php
+++ update/index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <!--Need to: -->
-    <title>Update Page</title>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
-      charset="UTF-8"
-    />
-  </head>
-  <body>
-    <button class="UpdateButton" onclick="run=true">
-        Update
-    </button>
-  </body>
-</html>
-
 <?php
-if ($_GET['run']) {
-shell_exec("update.sh");
-header('Location: https://www.74services.net/update');
-}
+exec("update.sh");
 ?>
