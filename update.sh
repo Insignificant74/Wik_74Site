aca986dbe2c275d925abf249f2ec219758e91be8
OreoRedstone <isaac.the.owen@gmail.com>
2024-08-07 12:15:40 +0100

    istg php version will be the death of me

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ cd /usr/share/nginx/html/Wik_74Site
 git pull
 mv -f nginx.conf /etc/nginx/nginx.conf
 service nginx restart
-service php-fpm restart
+service php7-fpm restart
 nginx -g 'daemon off;'
