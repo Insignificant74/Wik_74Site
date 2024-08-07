94fc6db7099ca5c4d1966daf2ec90869c62dd038
OreoRedstone <isaac.the.owen@gmail.com>
2024-08-07 11:06:33 +0100

    Fixes

--- Assets/Update_JS.js
+++ Assets/Update_JS.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const UpdateButton = document.querySelector(".UpdateButton");
   UpdateButton.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "Update_Site.php");
+    xhr.open("POST", "Update_PHP.php");
     xhr.onload = function () {
       console.log(this.response);
     };
