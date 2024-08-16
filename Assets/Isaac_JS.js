69638e69e7b327e263cb09f5340f800c0e57baf4
Insignificant74 <86426272+Insignificant74@users.noreply.github.com>
2024-08-16 20:01:19 +0100

    Start menu implemented + minor changes

--- Assets/Isaac_JS.js
+++ Assets/Isaac_JS.js
@@ -1,4 +1,10 @@
 window.onload = function () {
+  const StartButton = document.getElementById("StartButton");
+  const StartMenu = document.getElementById("StartMenu");
+  StartButton.addEventListener("click", function () {
+    StartButton.classList.toggle("is-active");
+    StartMenu.classList.toggle("is-active");
+  });
   Clock.call();
 };
 
@@ -12,14 +18,18 @@ function Clock() {
   if (hours > 12) {
     hours -= 12;
     meridiem = "PM";
-  } else if ((hours == 12)) {
+  } else if (hours == 12) {
     meridiem = "PM";
-  } else if ((hours == 0)) {
+  } else if (hours == 0) {
     hours = 12;
   }
 
-  if (minutes < 10){
-    minutes = '0' + minutes;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
   }
 
   Time.innerHTML = hours + ":" + minutes + " " + meridiem;
