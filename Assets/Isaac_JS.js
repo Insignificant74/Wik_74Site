b1f64f7ed829b180fc3e9a1123a141d2cd3c60f5
Insignificant74 <86426272+Insignificant74@users.noreply.github.com>
2024-08-25 06:53:29 +0100

    Start Menu update

--- Assets/Isaac_JS.js
+++ Assets/Isaac_JS.js
@@ -1,9 +1,80 @@
 window.onload = function () {
   const StartButton = document.getElementById("StartButton");
   const StartMenu = document.getElementById("StartMenu");
+  const Programs = document.getElementById("Menu_Programs");
+  const Documents = document.getElementById("Menu_Documents");
+  const Settings = document.getElementById("Menu_Settings");
+  const Find = document.getElementById("Menu_Find");
+  const Help = document.getElementById("Menu_Help");
+  const Run = document.getElementById("Menu_Run");
+  const Shut_Down = document.getElementById("Menu_Shut_Down");
+
   StartButton.addEventListener("click", function () {
     StartButton.classList.toggle("is-active");
     StartMenu.classList.toggle("is-active");
+  });
+  Programs.addEventListener("mouseover", function () {
+    Programs.classList.add("is-active");
+    Documents.classList.remove("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Documents.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.add("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Settings.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.remove("is-active");    
+    Settings.classList.add("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Find.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.remove("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.add("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Help.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.remove("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.add("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Run.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.remove("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.add("is-active");
+    Shut_Down.classList.remove("is-active");
+  });
+  Shut_Down.addEventListener("mouseover", function () {
+    Programs.classList.remove("is-active");
+    Documents.classList.remove("is-active");
+    Settings.classList.remove("is-active");
+    Find.classList.remove("is-active");
+    Help.classList.remove("is-active");
+    Run.classList.remove("is-active");
+    Shut_Down.classList.add("is-active");
   });
   Clock.call();
 };
